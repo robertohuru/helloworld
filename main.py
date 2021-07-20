686bf2f15eb08f75725527299aeabe1eded5580b
rohuru <robertohuru@gmail.com>
2021-07-20 18:06:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-def add_numbers(num1, num2):
-  return num1 + num2
 
+class Numerics:
+
+    def __init__(self):
+        pass
+
+    def add(self, num1, num2):
+        """
+        Add two numbers
+        """
+
+        return num1 + num2
+
+
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        num = Numerics()
+        self.assertEqual(num.add(5, 5), 10)
+
+    def test_isupper(self):
+        num = Numerics()
+        self.assertTrue(1 == num.add(1, 0))
+        self.assertFalse(0 == num.add(1, 0))
+
+
+if __name__ == '__main__':
+    unittest.main()
