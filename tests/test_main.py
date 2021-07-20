aec18c0f011b32be0c351c769a9ff52dbf78dafe
rohuru <robertohuru@gmail.com>
2021-07-20 22:21:25 +0200

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+class Numerics:
+
+    def __init__(self):
+        pass
+
+    def add(self, num1, num2):
+        """
+        Add two numbers
+        """
+
+        return num1 + num2
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        num = Numerics()
+        self.assertEqual(num.add(5, 5), 10)
+
+    def test_isupper(self):
+        num = Numerics()
+        self.assertTrue(1 == num.add(1, 0))
+        self.assertFalse(0 == num.add(1, 0))
+
+
+if __name__ == '__main__':
+    unittest.main()
